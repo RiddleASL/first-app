839009f9f085926de13b8ded71bb10f871e99532
RiddleASL <n00212142@iadt.ie>
2022-09-30 12:00:03 +0100

    LinkedIn Learning - Intro - Chpt 3 (Using Eloquent)

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts/base')
+
+@section('name', 'Aaron')
+
+@section('content')
+    <h1>About</h1>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<x-base>
+
+<x-slot name="name">
+    Aaron
+</x-slot>
+
+<x-slot name="content">
+    <h1>Home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia suscipit voluptates tenetur at enim beatae ea vel voluptatum nobis explicabo aut odio ad, eligendi eum impedit? Eum adipisci sit et?</p>
+</x-slot>
+
+</x-base>
